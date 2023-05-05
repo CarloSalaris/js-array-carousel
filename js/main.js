767fb1a4780e866904d8810f3ec4f8ee2fdfebbc
CarloSalaris <carloa.salaris@gmail.com>
2023-05-05 17:43:37 +0200

    MILESTONE 2 Commented imgs divs in HTML and added them dynamically through JS

--- js/main.js
+++ js/main.js
@@ -7,7 +7,32 @@
 
 // SVILUPPO CONSEGNA
 // creare array
+const images = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp"
+];
+
+const itemContainer = document.getElementById("itemContainer");
+
 // creare ciclo for per passare tutti gli elementi dell'array
+for (let i = 0; i < images.length; i++) {
+    
     // inserire elementi HTML con template literal
-    // inserire classi item (nascosto) + active (visibile)
+    itemContainer.innerHTML +=
+    `<div class="item">
+        <img src="${images[i]}" alt="image">
+    </div>`;
+}
+
+console.log(itemContainer);
+
+let activePosition = 0;
+
+const itemContainerList = document.querySelectorAll("#itemContainer .item");
+itemContainerList[activePosition].classList.add("active");
+console.log(itemContainerList);
+// inserire classi item (nascosto) + active (visibile)
 
