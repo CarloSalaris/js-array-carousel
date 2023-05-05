fb5c4d6fa873773e5c1017a57024d539b7d6c1ba
CarloSalaris <carloa.salaris@gmail.com>
2023-05-05 16:15:34 +0200

    MILESTONE 2 Commenti struttura JS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* MILESTONE 2 */
+
+// CONSEGNA
+// Inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal.
+
+// Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. (Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.)
+
+// SVILUPPO CONSEGNA
+// creare array
+// creare ciclo for per passare tutti gli elementi dell'array
+    // inserire elementi HTML con template literal
+    // inserire classi item (nascosto) + active (visibile)
+
